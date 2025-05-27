c059d04b99956b9d8f74d3117886a0bde22d3941
Geovana <geovanacvalentimb@gmail.com>
2025-05-27 20:30:46 -0300

    atualizações html e css

--- Views/home.php
+++ Views/home.php
@@ -3,13 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <title>MyWallet - Home</title>
-  <link rel="icon" href="/img/icon.png" type="image/png">
-
+  <link rel="icon" href="img/icon.png" type="image/png">
   <link rel="stylesheet" href="CSS/home.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poltawski+Nowy:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-  <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
 </head>
 <body>
   <div class="container">
