9bc28bd51fa9a43cd067c88c0aec26eae6614db7
GEOVANA CRISTINA VALENTIM BUENO <geovanacvalentimb@gmail.com>
2025-05-26 21:33:16 -0300

    Home html e css

---
+++ Views/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>MyWallet - Home</title>
+  <link rel="icon" href="/img/icon.png" type="image/png">
+
+  <link rel="stylesheet" href="CSS/home.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poltawski+Nowy:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <div class="left">
+      <h1 class="title">MyWallet</h1>
+      <br>
+      <h2>Seja bem-vindo!</h2>
+      <br>
+      <p>
+        Aqui você acompanha seu saldo em tempo real,<br>
+        visualiza um resumo financeiro e ainda pode gerar<br>
+        relatórios em PDF para <strong>manter tudo sob controle.</strong>
+      </p>
+      <br>
+      <button>Comece agora mesmo!</button>
+    </div>
+    <div class="right">
+      <img src="img/dim-moedinha.png" alt="dinheiro e moeda">
+    </div>
+  </div>
+</body>
+</html>
